4e19d865a095429607723b2d32eeabd0b8eee6fb
Andrei Dobrinski <andreiidobrinski@gmail.com>
2021-09-29 16:34:26 -0400

    state reducer - before

---
+++ state-reducer/after.jsx
@@ -0,0 +1,139 @@
+import React from "react";
+
+// Wizard
+
+const WizardContext = React.createContext();
+
+const defaultInitialState = {
+  activePageIndex: 0,
+  steps: 0
+};
+
+const actions = {
+  NEXT_PAGE: 'NEXT_PAGE',
+  PREV_PAGE: 'PREV_PAGE',
+  SET_STEPS: 'SET_STEPS',
+}
+
+const defaultReducer = (state, action) => {
+  const { activePageIndex, steps } = state;
+  switch(action.type) {
+    case actions.NEXT_PAGE:
+      return { ...state, activePageIndex: activePageIndex + 1 };
+    case actions.PREV_PAGE:
+      return { ...state, activePageIndex: activePageIndex - 1 };
+    case actions.SET_STEPS:
+      return { ...state, steps: action.payload };
+    default:
+      return state;
+  }
+}
+
+const Wizard = ({ children }) => {
+  const [activePageIndex, setActivePageIndex] = React.useState(0);
+  const [steps, setSteps] = React.useState(0);
+  const [state, dispatch] = React.useReducer(
+    defaultReducer,
+    defaultInitialState
+  )
+
+  const goNextPage = () => {
+    dispatch({ type: actions.NEXT_PAGE });
+  };
+
+  const goPrevPage = () => {
+    dispatch({ type: action.PREV_PAGE });
+  };
+
+  const setSteps = React.useCallback((n) => {
+    dispatch({ type: actions.SET_STEPS, payload: n });
+  }, [dispatch]);
+
+  const context = {
+    activePageIndex,
+    goNextPage,
+    goPrevPage,
+    steps,
+    setSteps
+  };
+
+  return (
+    <WizardContext.Provider value={context}>
+      <div>{children}</div>
+    </WizardContext.Provider>
+  );
+};
+
+export const WizardPages = (props) => {
+  const { activePageIndex, setSteps } = React.useContext(WizardContext);
+  const pages = React.Children.toArray(props.children);
+  const steps = React.Children.count(props.children);
+  React.useEffect(() => {
+    setSteps(steps)
+  }, [steps, setSteps]);
+  const currentPage = pages[activePageIndex];
+  return <div {...props}>{currentPage}</div>;
+};
+
+
+export const WizardButtonPrev = (props) => {
+  const { goPrevPage, activePageIndex } = React.useContext(WizardContext);
+  return activePageIndex > 0 ? (
+    <button type="button" {...props} onClick={goPrevPage}>
+      Back
+    </button>
+  ) : null;
+};
+
+export const WizardButtonNext = (props) => {
+  const { goNextPage, activePageIndex, steps } = React.useContext(
+    WizardContext
+  );
+  return activePageIndex < steps - 1 ? (
+    <button type="button" {...props} onClick={goNextPage}>
+      Next
+    </button>
+  ) : null;
+};
+
+
+Wizard.ButtonNext = WizardButtonNext;
+Wizard.ButtonPrev = WizardButtonPrev;
+Wizard.Pages = WizardPages;
+
+// Pages
+
+const Page1 = () => (
+  <div>
+    <h1>Pagina 1</h1>
+  </div>
+);
+
+const Page2 = () => (
+  <div>
+    <h1>Pagina 2</h1>
+  </div>
+);
+const Page3 = () => (
+  <div>
+    <h1>Pagina 3</h1>
+  </div>
+);
+
+// App
+
+const App = () => {
+  return (
+    <Wizard>
+      <Wizard.Pages className="wizard__content">
+        <Page1 />
+        <Page2 />
+        <Page3 />
+      </Wizard.Pages>
+      <div className="wizard__buttons">
+        <Wizard.ButtonPrev className="wizard__buttons-left" />
+        <Wizard.ButtonNext className="wizard__buttons-right" />
+      </div>
+    </Wizard>
+  );
+};
